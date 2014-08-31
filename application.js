b3fe61ad7cc9fa4b20afd0ba19d2ab2da9ccd7b9
prrashi <prashanth.pamidi@gmail.com>
2014-08-30 23:15:16 -0400

    Changed code according to new version

---
+++ application.js
@@ -0,0 +1,38 @@
+$(function () {
+
+  var rating = 3.6;
+
+  $(".counter").text(rating);
+
+  var changeRating = function (rating) {
+
+    $(this).next().text(rating);
+  };
+
+  $("#rateYo1").rateYo({
+    rating: 3.6,
+    onSet: changeRating,
+    onChange: changeRating
+  });
+
+  $("#getRating").click(function (e) {
+
+    e.preventDefault();
+
+    var rating = $("#rateYo1").rateYo("options", "rating");
+
+    window.alert("Its " + rating + "!");
+  });
+
+  $("#setRating").click(function (e) {
+
+    e.preventDefault();
+
+    var randomRating = parseFloat((Math.random()*5 + 1).toFixed(2));
+
+    randomRating = randomRating>5?5:randomRating;
+
+    $("#rateYo1").rateYo("options", "rating", randomRating);
+  });
+});
+
