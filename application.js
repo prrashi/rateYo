96d514607f7efe1f9384e32bfd587e34740b0c20
prrashi <prashanth.pamidi@gmail.com>
2014-09-22 14:18:50 -0400

    Added starWidth description to the doc

--- application.js
+++ application.js
@@ -1,12 +1,37 @@
 $(function () {
 
-  var rating = 3.6;
+  var rating = 3.6
+
+  function getRandomRating () {
+
+    var randomRating = parseFloat((Math.random()*5 + 1).toFixed(2));
+
+    randomRating = randomRating>5?5:randomRating;
+
+    return randomRating;
+  };
 
   $("#rateYo").rateYo({
 
     rating: rating
   });
 
+  var $options = $("div.options-content div.option");
+
+  /* Option - StarWidth */
+  var $rateYos = $options.filter(".option-starWidth").find("div.rating");
+
+  $rateYos.each(function () {
+
+    var width = $(this).attr("data-width");
+
+    $(this).rateYo({
+
+      rating: getRandomRating(),
+      starWidth: width
+    });
+  });
+  /*
   $(".counter").text(rating);
 
   var changeRating = function (rating) {
@@ -33,11 +58,10 @@ $(function () {
 
     e.preventDefault();
 
-    var randomRating = parseFloat((Math.random()*5 + 1).toFixed(2));
-
-    randomRating = randomRating>5?5:randomRating;
+    var randomRating = getRandomRating();
 
     $("#rateYo1").rateYo("method", "rating", randomRating);
   });
+  */
 });
 
