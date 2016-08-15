ed61836d8f7578ad954cb3e1758a8c1ca77b9b9d
prrashi <ninja@fundoocode.ninja>
2016-08-15 23:03:46 +0530

    Added starSvg Hack option to the doc

--- application.js
+++ application.js
@@ -279,5 +279,20 @@ $(function () {
 
     $(this).next().text(data.rating);
   });
+
+  var $starSvgDemo = $("#hack-starSvg div.rating");
+
+  $starSvgDemo.rateYo({
+
+    "rating" : 3.2,
+    "starSvg": "<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'>"+
+                 "<path d='M12 6.76l1.379 4.246h4.465l-3.612 2.625 1.379"+
+                           " 4.246-3.611-2.625-3.612 2.625"+
+                           " 1.379-4.246-3.612-2.625h4.465l1.38-4.246zm0-6.472l-2.833"+
+                           " 8.718h-9.167l7.416 5.389-2.833 8.718 7.417-5.388"+
+                           " 7.416 5.388-2.833-8.718"+
+                           " 7.417-5.389h-9.167l-2.833-8.718z'/>"+
+                "</svg>"
+  });
 });
 
